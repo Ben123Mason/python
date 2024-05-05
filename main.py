7f31372b0c653d844fc52c96ee2e133d58e1f781
Ben123Mason <160073273+Ben123Mason@users.noreply.github.com>
2024-05-06 11:59:19 +1200

    added question

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # ask the user their name and store it
+name =input("What is your name?")
 #greet the user and introduce the quiz
+print("Welcome to this quiz",name)
+print("This quiz is about capital citys of the world")
 #ask the user a question
-#tell them the correct answer
+answer=input("What is the capital city of New Zealand?")
+#check the user's answer and give feedback
+if answer== "wellington" input ("correct")
+print("Wellington")
 #end the quiz
+print("thank you for playing")
